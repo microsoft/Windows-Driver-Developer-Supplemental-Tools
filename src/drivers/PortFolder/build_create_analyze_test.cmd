ccd2034534eda87be3d79a138fadde94bbcea6fd
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2022-08-11 15:05:14 -0700

    Add OpaqueMdl baselines and tweak test script

--- src/drivers/PortFolder/build_create_analyze_test.cmd
+++ src/drivers/PortFolder/build_create_analyze_test.cmd
@@ -19,11 +19,13 @@ msbuild /t:rebuild /p:platform=x64
 @REM the "..\..\TestDB\%1" in the command below specifies a location for the database we want to create. The %1 will correspond to the the 
 @REM first argument of the call above, aka, PendingStatusError in this case. 
 echo creating_database
+mkdir ..\..\TestDB
 codeql database create -l=cpp -c "msbuild /p:Platform=x64 /t:rebuild" "..\..\TestDB\%1" 
 
 @REM Similar to the case above, the %1 corresponds to PendingStatusError
 cd ..\..
 echo analysing_database
+mkdir "AnalysisFiles\Test Samples"
 codeql database analyze "TestDB\%1" --format=sarifv2.1.0 --output="AnalysisFiles\Test Samples\%1.sarif" "..\wdm\queries\%1\%1.ql" 
 
 echo comparing analysis result with expected result
