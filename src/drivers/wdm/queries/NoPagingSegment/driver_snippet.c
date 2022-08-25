a8436ae6a88d0d4f564e175124ca647b467787a9
t-abtamene <108037016+t-abtamene@users.noreply.github.com>
2022-08-25 16:03:08 -0700

    Abenezer/c28170 c28171 c28172 (#26)
    * page queries
    
    * c28171 ported
    
    * true positives for NoPagedCode
    
    * c28170 ported
    
    * test file for c28172
    
    * page queries ported
    
    * page queries port
    
    * page.qll update
    
    * page queries ported
    
    * removing KMDF test template as it's part of a previous PR
    
    * minor touches
    
    * suite update
    
    * nate's suggestions
    
    * merge conflic fix
    
    * merge conflict fix
    
    * fix typo
    
    Signed-off-by: t-abtamene <108037016+t-abtamene@users.noreply.github.com>

---
+++ src/drivers/wdm/queries/NoPagingSegment/driver_snippet.c
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+
+//Macros to enable or disable a code section that may or maynot conflict with this test.
+#define SET_DISPATCH 1
+#define SET_PAGE_CODE 1
+
+
+_Dispatch_type_(IRP_MJ_CLEANUP) 
+DRIVER_DISPATCH DispatchCleanup;
+
+_Dispatch_type_(IRP_MJ_SHUTDOWN)
+DRIVER_DISPATCH DispatchShutdown;
+
+#ifndef __cplusplus
+#pragma alloc_text (PAGE, DispatchCleanup)
+#endif
+
+
+//Template
+void top_level_call(){
+}
+
+//Passes
+NTSTATUS
+DispatchCleanup (
+    PDEVICE_OBJECT DriverObject,
+    PIRP Irp
+    )
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(Irp);
+    PAGED_CODE();
+    
+    return STATUS_SUCCESS;
+}
+
+//Fails
+NTSTATUS
+DispatchShutdown (
+    PDEVICE_OBJECT DriverObject,
+    PIRP Irp
+    )
+{
+    UNREFERENCED_PARAMETER(DriverObject);
+    UNREFERENCED_PARAMETER(Irp);
+    PAGED_CODE();
+    
+    return STATUS_SUCCESS;
+}
+
+
+#pragma code_seg("PAGE")
+//Passes
+NTSTATUS func1(){
+    PAGED_CODE();
+    if(TRUE){
+    }
+    return STATUS_SUCCESS;
+}
+#pragma code_seg()
+
+//Fails
+NTSTATUS func2(){
+    PAGED_CODE();
+    return STATUS_SUCCESS;
+}
+
+#define PAGED_CODE_SEG __declspec(code_seg("PAGE"))
+//Passes
+PAGED_CODE_SEG
+NTSTATUS func3(){
+    PAGED_CODE();
+    return STATUS_SUCCESS;
+}
