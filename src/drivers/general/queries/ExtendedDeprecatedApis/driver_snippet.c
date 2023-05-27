1f42e932f47ac46b6ea3605d91864a4f9bfd6c34
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2023-05-26 17:48:25 -0700

    Create a library and query for converting Code Analysis-style suppressions to CodeQL suppressions. (#68)
    * Initial suppression experiments.
    
    * Additional suppression pragma development.
    
    * Update CodeQL packs to match CLI 2.11.0.
    
    * Further bump to match 2.11.5
    
    * Finish proof-of-concept suppression query.
    
    Still needs cleanup.
    
    * Add additional CA-to-CodeQL suppression mappings.
    
    * Additional formatting + cases for suppression
    
    * Cleanup/comments/test code
    
    * Update pop/push to use regexes
    
    * Re-add missing QLPack files
    
    This is just to avoid diff.

--- src/drivers/general/queries/ExtendedDeprecatedApis/driver_snippet.c
+++ src/drivers/general/queries/ExtendedDeprecatedApis/driver_snippet.c
@@ -13,14 +13,49 @@ void top_level_call() {}
 VOID ExtendedDeprecatedApis_Pass()
 {
     PSTR src = "Passing case!";
-    char dst[100]; 
-    strcpy_s(dst, sizeof(dst), src); 
+    char dst[100];
+    strcpy_s(dst, sizeof(dst), src);
 }
 
 // Failing case: includes calls to deprecated APIs
 VOID ExtendedDeprecatedApis_Fail()
 {
     PSTR src = "Failing case, even though we have enough space!";
-    char dst[100]; 
-    strcpy(dst, src); 
+    char dst[100];
+    strcpy(dst, src);
+}
+
+/*  Test code for AlertSuppression.ql.
+    Because alert suppression queries don't directly output to SARIF,
+    this code should be built as a database and then have both ExtendedDeprecatedApis and
+    DriverAlertSuppression.ql run to test suppression. */
+VOID AlertSuppressionTesting()
+{
+    PSTR src = "Testing suppression";
+    char dst[100];
+
+    strcpy(dst, src); // Unsuppressed
+
+#pragma prefast(suppress : 28719)
+    strcpy(dst, src);
+#pragma prefast(suppress : 28719, "Suppression with comment")
+    strcpy(dst, src);
+#pragma prefast(suppress : __WARNING_BANNED_API_USAGE, "Suppression with name and comment")
+    strcpy(dst, src);
+#pragma prefast(suppress : cpp/windows/drivers/queries/extended-deprecated-apis)
+    strcpy(dst, src);
+#pragma prefast(suppress : 28719)
+#pragma prefast(suppress : 28720)
+    strcpy(dst, src); // BUG: Should be suppressed, isn't
+#pragma prefast(suppress : 28720 28719)
+    strcpy(dst, src); // BUG: Should be suppressed, isn't
+#pragma prefast(disable : 28719)
+    strcpy(dst, src); // BUG: Should be suppressed, isn't
+#pragma prefast(push)
+    strcpy(dst, src); // Unsuppressed
+
+#pragma prefast(disable : 28719)
+    strcpy(dst, src);
+#pragma prefast(pop)
+    strcpy(dst, src); // BUG: Should be suppressed, isn't
 }
