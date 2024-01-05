6cc0aed80ee22be185a389ccae641b13996d64b0
jacob-ronstadt <jacobronstadt@pm.me>
2024-01-05 14:06:38 -0800

    update build script with option to run queries on existing database. Add requirements.txt for installing dependencies

--- src/drivers/test/build_create_analyze_test.py
+++ src/drivers/test/build_create_analyze_test.py
@@ -1,21 +1,18 @@
-from multiprocessing.pool import ThreadPool
-import os
-import sys
-import subprocess
-import shutil
-import threading
-import time
-import os
-import sys
-import subprocess
-import shutil
-import threading
-import time
-from multiprocessing.pool import ThreadPool
-import fnmatch
+try:
+    from multiprocessing.pool import ThreadPool
+    import os
+    import sys
+    import subprocess
+    import shutil
+    import threading
+    import time
+    import fnmatch
+    import sarif # Not directly used in the script but required for the sarif diff command. Importing it here checks for the module
+except ImportError as e:
+    print("Import error: " + str(e) + "\nPlease install the required modules using pip install -r requirements.txt")
+    exit(1)
 
-no_output = False
-override_template = ""
+
 
 print_mutex = threading.Lock()
 
@@ -79,6 +76,16 @@ def usage():
     print("-i <name>: run only the tests with <name> in the name")
     print("-t <num_threads>: run multithreaded with max <num_threads> threads")
     print("-o: output off")
+    print("--override_template <template>: override the template used for the test")
+    print("--use_codeql_repo <path>: use the codeql repo at <path> for the test instead of qlpack installed with CodeQL Package Manager")
+    print("--database <path>: Run all queries using the database at <path>")
+
+
+
+def get_git_root():
+    git_root = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
+    git_root=git_root.split('/')[-1]
+    return git_root
 
 def check_use_ntifs(ds_file):
     file = open(ds_file, "r")
@@ -93,7 +100,6 @@ def check_use_ntifs(ds_file):
 # walk through files and look for .ql files
 def walk_files(directory, extension):
     ql_files_map = {}
-    # print(directory)
     for root, dirs, files in os.walk(directory):
         if fnmatch.filter(files, "driver_snippet.*"):
             use_ntifs = check_use_ntifs(os.path.join(root, fnmatch.filter(files, "driver_snippet.*")[0]))
@@ -108,18 +114,7 @@ def walk_files(directory, extension):
 
     return ql_files_map
 
-
-
-# Function to run a test using system calls
-def run_test(ql_test):
-    # Print test attributes
-    print_mutex.acquire()
-    print(ql_test.get_ql_name(), "\n",  
-          "Template: ", ql_test.get_template(), "\n",  
-          "Driver Framework: ", ql_test.get_ql_type(), "\n",  
-          "Query location: ", ql_test.get_ql_location(), "\n",  
-          "UseNTIFS flag: ", ql_test.get_use_ntifs(),"\n")
-    print_mutex.release()
+def test_setup(ql_test):  
     # Remove existing working directory
     if os.path.exists(os.path.join(os.getcwd(), "working/"+ql_test.get_ql_name()+'/').strip()):
         shutil.rmtree(os.path.join(os.getcwd(), "working/"+ql_test.get_ql_name()+'/'))
@@ -135,7 +130,10 @@ def run_test(ql_test):
     out1 = subprocess.run(["msbuild", "/t:rebuild", "/p:platform=x64", "/p:UseNTIFS="+ql_test.get_use_ntifs()+""],cwd=os.path.join(os.getcwd(),"working\\"+ql_test.get_ql_name()), shell=True, capture_output=no_output  ) 
     if not no_output and out1.returncode != 0:
         print("Error in msbuild: " + ql_test.get_ql_name())
-        return
+        return None
+    return out1
+
+def create_codeql_database(ql_test):
     # Create the CodeQL database
     os.makedirs("TestDB", exist_ok=True) 
     if os.path.exists("TestDB\\"+ql_test.get_ql_name()):
@@ -145,43 +143,100 @@ def run_test(ql_test):
                     shell=True, capture_output=no_output  ) 
     if not no_output and out2.returncode != 0:
         print("Error in codeql database create: " + ql_test.get_ql_name())
-        return
+        return None
+    return out2
+
+def analyze_codeql_database(ql_test):
+      
     # Analyze the CodeQL database
     if not os.path.exists("AnalysisFiles\Test Samples"):
         os.makedirs("AnalysisFiles\Test Samples", exist_ok=True) 
-    out3 = subprocess.run(["codeql", "database", "analyze", "TestDB\\"+ql_test.get_ql_name(), "--format=sarifv2.1.0", "--output=AnalysisFiles\\Test Samples\\"+ql_test.get_ql_name()+".sarif", "..\\"+ql_test.get_ql_type()+"\\"+ql_test.get_ql_location()+"\\"+ql_test.get_ql_name()+"\*.ql" ], 
+    
+    if existing_database:
+        database_loc = existing_database_path
+    else:
+        database_loc = "TestDB\\"+ql_test.get_ql_name()
+
+    if use_codeql_repo:
+        out3 = subprocess.run(["codeql", "database", "analyze", database_loc, "--format=sarifv2.1.0", "--output=AnalysisFiles\\Test Samples\\"+ql_test.get_ql_name()+".sarif", "..\\"+ql_test.get_ql_type()+"\\"+ql_test.get_ql_location()+"\\"+ql_test.get_ql_name()+"\*.ql", "--additional-packs", codeql_repo_path], 
                     shell=True, capture_output=no_output  ) 
+    else:
+        out3 = subprocess.run(["codeql", "database", "analyze", database_loc, "--format=sarifv2.1.0", "--output=AnalysisFiles\\Test Samples\\"+ql_test.get_ql_name()+".sarif", "..\\"+ql_test.get_ql_type()+"\\"+ql_test.get_ql_location()+"\\"+ql_test.get_ql_name()+"\*.ql" ], 
+                    shell=True, capture_output=no_output  ) 
+        
     if not no_output and out3.returncode != 0:
         print("Error in codeql database analyze: " + ql_test.get_ql_name())
-        return
+        return None
+    return out3
+
+
+def sarif_diff(ql_test):
     # Perform SARIF diff
     out4 = subprocess.run(["sarif", "diff", "-o", "diff\\"+ql_test.get_ql_name()+".sarif", "..\\"+ql_test.get_ql_type()+"\\"+ql_test.get_ql_location()+"\\"+ql_test.get_ql_name()+"\\"+ql_test.get_ql_name()+".sarif", "AnalysisFiles\Test Samples\\"+ql_test.get_ql_name()+".sarif"], 
                     shell=True, capture_output=no_output  ) 
     if not no_output and out4.returncode != 0:
         print("Error in sarif diff: " + ql_test.get_ql_name())
-        return
+        return None
+    return out4
     
+
+# Function to run a test using system calls
+def run_test(ql_test):
+    # Print test attributes
+    print_mutex.acquire()
+    print(ql_test.get_ql_name(), "\n",  
+          "Template: ", ql_test.get_template(), "\n",  
+          "Driver Framework: ", ql_test.get_ql_type(), "\n",  
+          "Query location: ", ql_test.get_ql_location(), "\n",  
+          "UseNTIFS flag: ", ql_test.get_use_ntifs(),"\n")
+    print_mutex.release()
+
+    
+    if not existing_database:
+        test_setup_result = test_setup(ql_test)
+        if test_setup_result is None:
+            return 
+    
+        create_codeql_database_result = create_codeql_database(ql_test)
+        if create_codeql_database_result is None:
+            return 
+    
+    analyze_codeql_database_result = analyze_codeql_database(ql_test)
+    if analyze_codeql_database_result is None:
+        return
+
+    if not existing_database:   
+        sarif_diff_result = sarif_diff(ql_test)
+        if sarif_diff_result is None: 
+            return
+
     # Check for errors
-    if(no_output):
+    if no_output and not existing_database:
         print_mutex.acquire()
-        if(out1.returncode != 0 or out2.returncode != 0 or out3.returncode != 0 or out4.returncode != 0):
+        if (test_setup_result.returncode != 0 or create_codeql_database_result.returncode != 0 or
+            analyze_codeql_database_result.returncode != 0 or sarif_diff_result.returncode != 0):
             print("Error in test: " + ql_test.get_ql_name())
 
-            if(out1.returncode != 0):
+            if test_setup_result.returncode != 0:
                 print("Error in msbuild: " + ql_test.get_ql_name())
-                print(out1.stderr.decode())
-            if(out2.returncode != 0):
+                print(test_setup_result.stderr.decode())
+            if create_codeql_database_result.returncode != 0:
                 print("Error in codeql database create: " + ql_test.get_ql_name())
-                print(out2.stderr.decode())
-            if(out3.returncode != 0):
+                print(create_codeql_database_result.stderr.decode())
+            if analyze_codeql_database_result.returncode != 0:
                 print("Error in codeql database analyze: " + ql_test.get_ql_name())
-                print(out3.stderr.decode())
-            if(out4.returncode != 0):
+                print(analyze_codeql_database_result.stderr.decode())
+            if sarif_diff_result.returncode != 0:
                 print("Error in sarif diff: " + ql_test.get_ql_name())
-                print(out4.stderr.decode())
+                print(sarif_diff_result.stderr.decode())
         else:
             print("Test complete: " + ql_test.get_ql_name())
         print_mutex.release()
+    else:
+        if analyze_codeql_database_result.returncode != 0:
+            print("Error in codeql database analyze: " + ql_test.get_ql_name())
+            print(analyze_codeql_database_result.stderr.decode())
+      
 
 def parse_attributes(queries):
     query_objs = []
@@ -229,15 +284,27 @@ def run_tests(ql_tests_dict):
         run_test(ql_test)
         
 if __name__ == "__main__":
+
+    # Sys input flags
+    no_output = False
+    override_template = ""
+    name = ""
+    use_codeql_repo = False
+    codeql_repo_path = ""
+    existing_database_path = ""
+    existing_database = False
+
     start_time = time.time()
     subprocess.run(["clean.cmd"]) 
     cwd = os.getcwd()
     path = os.path.normpath(cwd)
     path = path.split(os.sep)
-    dir_to_search ="/".join(path[0:path.index("Windows-Driver-Developer-Supplemental-Tools")+1])
+    root_dir = get_git_root()
+    
+    dir_to_search ="/".join(path[0:path.index(root_dir)+1])
 
     ql_tests = walk_files(dir_to_search, ".ql")
-
+    
     threads = []    
     if "-h" in sys.argv:
         usage()
@@ -246,17 +313,27 @@ if __name__ == "__main__":
             no_output = True
         if "--override_template" in sys.argv:
             override_template = sys.argv[sys.argv.index("--override_template")+1]
+        if "--use_codeql_repo" in sys.argv:
+            use_codeql_repo = True
+            codeql_repo_path = sys.argv[sys.argv.index("--use_codeql_repo")+1]
+        if "--database" in sys.argv:
+            existing_database_path = sys.argv[sys.argv.index("--database")+1]
+            existing_database = True
+            if not os.path.exists(existing_database):
+                print("Database path does not exist")
+                exit(1)
+
         if "-i" in sys.argv:
             name = sys.argv[sys.argv.index("-i")+1]
             ql_files_keys = [x for x in ql_tests if name in x]
-
         elif "-t" in sys.argv:
             ql_files_keys = [x for x in ql_tests]
         elif len(sys.argv) == 1:
             ql_files_keys = [x for x in ql_tests]
-        
-        ql_tests = {x:ql_tests[x] for x in ql_tests if x in ql_files_keys}
+        else:
+            ql_files_keys = [x for x in ql_tests]
 
+        ql_tests = {x:ql_tests[x] for x in ql_tests if x in ql_files_keys}
         if "-t" in sys.argv:
             num_threads = int(sys.argv[sys.argv.index("-t")+1])
             print("Running multithreaded")
