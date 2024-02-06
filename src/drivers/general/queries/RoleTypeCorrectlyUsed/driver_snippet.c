95576235b982c1102648715ad53544fa7b7d364b
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2024-02-05 17:34:36 -0800

    Fix behavior for RoleTypeCorrectlyUsed

--- src/drivers/general/queries/RoleTypeCorrectlyUsed/driver_snippet.c
+++ src/drivers/general/queries/RoleTypeCorrectlyUsed/driver_snippet.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-//Macros to enable or disable a code section that may or maynot conflict with this test.
+//Macros to enable or disable a code section that may or may not conflict with this test.
 #define SET_DISPATCH 1
 
 //Template function. Not used for this test.
