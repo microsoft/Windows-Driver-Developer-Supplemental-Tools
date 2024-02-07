b1e0f0c72a310701f4f9d9fdbdc165289bed2c3e
jacob-ronstadt <jacobronstadt@pm.me>
2024-02-07 12:06:57 -0800

    add unit tests for RoleTypeCorrectlyUsed

--- src/drivers/general/queries/RoleTypeCorrectlyUsed/driver_snippet.c
+++ src/drivers/general/queries/RoleTypeCorrectlyUsed/driver_snippet.c
@@ -1,10 +1,39 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-//Macros to enable or disable a code section that may or may not conflict with this test.
+// Macros to enable or disable a code section that may or may not conflict with this test.
 #define SET_DISPATCH 1
 
-//Template function. Not used for this test.
-void top_level_call(){
+// Template function. Not used for this test.
+void top_level_call()
+{
 }
 
+DRIVER_UNLOAD CsampUnload;
+VOID CsampUnload(
+    IN PDRIVER_OBJECT DriverObject)
+{
+    ; // do nothing
+}
+
+VOID BadUnload(
+    IN PDRIVER_OBJECT DriverObject)
+{
+    ; // do nothing
+}
+
+NTSTATUS
+DriverEntryPass(
+    PDRIVER_OBJECT DriverObject,
+    PUNICODE_STRING RegistryPath)
+{
+    DriverObject->DriverUnload = CsampUnload;
+}
+
+NTSTATUS
+DriverEntryFail(
+    PDRIVER_OBJECT DriverObject,
+    PUNICODE_STRING RegistryPath)
+{
+    DriverObject->DriverUnload = BadUnload;
+}
