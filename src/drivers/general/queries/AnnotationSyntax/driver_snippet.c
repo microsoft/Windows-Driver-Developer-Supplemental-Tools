3285eae3e7d9f76e4708234a5deb553f4a941445
jacob-ronstadt <jacobronstadt@pm.me>
2025-01-23 14:47:23 -0800

    CodeQL port of C28266

--- src/drivers/general/queries/AnnotationSyntax/driver_snippet.c
+++ src/drivers/general/queries/AnnotationSyntax/driver_snippet.c
@@ -9,4 +9,68 @@ void top_level_call()
 {
 }
 
-// TODO add tests for query
+// FAIL _IRQL_saves_global_ not applied to entire function
+VOID test1(
+    _IRQL_saves_global_(OldIrql, *Irql) PKIRQL Irql)
+{
+    // ...
+    ;
+}
+
+// FAIL using when with dispatch type
+_When_(return >= 0, __drv_dispatchType(IRP_MJ_CREATE))
+    VOID test2(
+        IN PDRIVER_OBJECT DriverObject)
+{
+    ; // do nothing
+}
+
+// FAIL
+_Function_class_(DRIVER_ADD_DEVICE)
+    _IRQL_requires_(PASSIVE_LEVEL)
+        _IRQL_requires_same_
+    _When_(return >= 0, _Kernel_clear_do_init_(IRP_MJ_CREATE))
+        NTSTATUS
+    test3(
+        _In_ PDRIVER_OBJECT DriverObject,
+        _In_ PDEVICE_OBJECT PhysicalDeviceObject)
+
+{
+    ; // do nothing
+}
+
+// OK
+_Function_class_(DRIVER_ADD_DEVICE)
+    _IRQL_requires_(PASSIVE_LEVEL)
+        _IRQL_requires_same_
+    _When_(return >= 0, _Kernel_clear_do_init_(__yes))
+        NTSTATUS
+    test3Ok(
+        _In_ PDRIVER_OBJECT DriverObject,
+        _In_ PDEVICE_OBJECT PhysicalDeviceObject)
+
+{
+    ; // do nothing
+}
+
+// FAIL
+_Function_class_(DRIVER_ADD_DEVICE)
+    _IRQL_requires_(PASSIVE_LEVEL)
+        _IRQL_requires_same_
+    _Kernel_clear_do_init_(IRP_MJ_CREATE)
+NTSTATUS
+test4(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PDEVICE_OBJECT PhysicalDeviceObject)
+
+{
+    ; // do nothing
+}
+
+// out of range
+__drv_dispatchType(65)
+    VOID test5(
+        IN PDRIVER_OBJECT DriverObject)
+{
+    ; // do nothing
+}
