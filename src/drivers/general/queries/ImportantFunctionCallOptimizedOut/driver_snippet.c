87c7a9d15bf15abb1e2d5e50c92ff07dea1e62c7
Jacob Ronstadt <147542405+jacob-ronstadt@users.noreply.github.com>
2025-02-07 15:44:20 -0800

    ImportantFunctionCallOptimizedOut: port of c28625 (#167)
    * Codeql port of C28625
    
    * Update src/drivers/general/queries/ImportantFunctionCallOptimizedOut/driver_snippet.c
    
    Co-authored-by: NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
    Signed-off-by: Jacob Ronstadt <147542405+jacob-ronstadt@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jacob Ronstadt <147542405+jacob-ronstadt@users.noreply.github.com>
    Co-authored-by: NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>

---
+++ src/drivers/general/queries/ImportantFunctionCallOptimizedOut/driver_snippet.c
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// Macros to enable or disable a code section that may or may not conflict with this test.
+#define SET_DISPATCH 1
+
+// Template function. Not used for this test.
+void top_level_call()
+{
+}
+#define ZeroMemory(a, b) memset(a, 0, b);
+
+void bad_func()
+{
+    char Password[100];
+
+    /*
+     * The Buffer will be going out of scope
+     * anyway so the compiler optimises away
+     * the following
+     */
+    ZeroMemory(Password, sizeof(Password));
+}
+void bad_func2()
+{
+    char Password[100];
+
+    /*
+     * The Buffer will be going out of scope
+     * anyway so the compiler optimises away
+     * the following
+     */
+    RtlZeroMemory(Password, sizeof(Password));
+}
+void good_func()
+{
+    char Password[100];
+
+    RtlSecureZeroMemory(Password, sizeof(Password));
+}
+char globalPassword[100];
+void good_func2()
+{
+    char * Password;
+    Password = globalPassword;
+    // ... do something with Password
+    RtlZeroMemory(Password, sizeof(globalPassword));
+}
